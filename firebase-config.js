c802b5397848ed0f805132480806922fd19f8b5e
whoisotu <ebose.otuekong@gmail.com>
2026-08-27 22:52:56 -0400

    Add Firebase multiplayer rooms

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAn6gGTtTi_PhIbMgddNrTzQB-jqJ0Z3jc",
+  authDomain: "math-clash-13c39.firebaseapp.com",
+  projectId: "math-clash-13c39",
+  storageBucket: "math-clash-13c39.firebasestorage.app",
+  messagingSenderId: "869238518923",
+  appId: "1:869238518923:web:44a25880e1b5ddf800908e"
+};
